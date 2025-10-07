36d8dfefc8f1c85d3a08daa79ce83b254a386bcd
DrAmir0240 <amirhosseinomidi1383@gmail.com>
2025-10-07 17:52:09 +0330

    adding faraz sms config

--- payments/models.py
+++ payments/models.py
@@ -8,9 +8,11 @@ class Transaction(models.Model):
     payer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='transactions_paid')
     receiver = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,
                                  related_name='transactions_received')
-    payment_method = models.CharField(max_length=100, blank=True, null=True)
+    payment_method = models.CharField(max_length=100,
+                                      choices=(('online', 'آنلاین'), ('cash', 'نقدی')),
+                                      default='online')
     online_transaction = models.CharField(max_length=255, blank=True, null=True)
-    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    price = models.IntegerField(default=0)
 
     def __str__(self):
         return f"Tx {self.id} - {self.price}"
